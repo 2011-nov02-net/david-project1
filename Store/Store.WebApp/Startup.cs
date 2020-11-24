af3b0f0d9de37a9add5fdafdfcf3201bc667fded
David Barnes <david.barnes0402@gmail.com>
2020-11-24 10:06:12 -0500

    Added project references

--- Store/Store.WebApp/Startup.cs
+++ Store/Store.WebApp/Startup.cs
@@ -4,10 +4,12 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Store.DataModel;
 
 namespace Store.WebApp
 {
@@ -23,6 +25,8 @@ namespace Store.WebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //db set up
+            services.AddDbContext<Project0Context>(options => options.UseSqlServer(Configuration.GetConnectionString("Project0Connection")));
             services.AddControllersWithViews();
         }
 
