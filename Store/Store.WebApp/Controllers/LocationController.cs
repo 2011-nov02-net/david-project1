47323d23b6630505c0064f1a70b4eacc48e2a906
David Barnes <david.barnes0402@gmail.com>
2020-11-25 14:41:29 -0500

    implements all location CRU operators

--- Store/Store.WebApp/Controllers/LocationController.cs
+++ Store/Store.WebApp/Controllers/LocationController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Store.Library.Repository_Interfaces;
+using Store.WebApp.ViewModels;
 
 namespace Store.WebApp.Controllers
 {
@@ -27,7 +28,8 @@ namespace Store.WebApp.Controllers
         // GET: Location/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            var location = _locationRepository.Get(id);
+            return View(location);
         }
 
         // GET: Location/Create
@@ -39,31 +41,63 @@ namespace Store.WebApp.Controllers
         // POST: Location/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create([Bind("Name")] LocationViewModel viewLocation, IFormCollection collection)
         {
+            if(!ModelState.IsValid)
+            {
+                return View(viewLocation);
+            }
+
             try
             {
+                // give the id as 1 just to get it into the system
+                // the create repo does not use that id to form a new DB entry
+                var location = new Library.Location(viewLocation.Name, 1);
+                _locationRepository.Create(location);
                 return RedirectToAction(nameof(Index));
             }
             catch
             {
-                return View();
+                ModelState.AddModelError("", "There was a problem Creating the Location");
+                return View(viewLocation);
             }
         }
 
         // GET: Location/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            // add the id to temp data for security
+            TempData["Location"] = id;
+            // get the location needed from the db
+            var location = _locationRepository.Get(id);
+            // convert to view model
+            var viewLocation = new LocationViewModel() { Name = location.Name, Id = location.Id };
+            return View(viewLocation);
         }
 
         // POST: Location/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(int id, [Bind ("Name, Id")] LocationViewModel viewLocation, IFormCollection collection)
         {
+            // get the current id from tempdata to ensure the user hasn't modified the id that the page put out
+            int tdId = (int)TempData.Peek("Location");
+            if (tdId != id && tdId != viewLocation.Id)
+            {
+                // *ER add error message
+                return RedirectToAction(nameof(Index));
+            }
+
+            // check modelstate
+            if(!ModelState.IsValid)
+            {
+                return View(viewLocation);
+            }
+
             try
             {
+                var location = new Library.Location(viewLocation.Name, viewLocation.Id);
+                _locationRepository.Update(location);
                 return RedirectToAction(nameof(Index));
             }
             catch
---
+++ Store/Store.WebApp/ViewModels/LocationViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Store.WebApp.ViewModels
+{
+    public class LocationViewModel
+    {
+        [Required, RegularExpression("[A-Z].*")]
+        public string Name { get; set; }
+        public int Id { get; set; }
+    }
+}
