fb2801cc96ed98156504a145592aeedf4c6ec9cd
David Barnes <david.barnes0402@gmail.com>
2020-11-30 15:00:08 -0500

    adds helper program to reduce code duplication

---
+++ Store/Store.WebApp/Helpers/Helpers.cs
@@ -0,0 +1,24 @@
+﻿using Store.Library;
+using Store.WebApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Store.WebApp.Helpers
+{
+    public class Helpers
+    {
+        public static List<OrderViewModel> ConvertOrdersToViewModel(IEnumerable<Order> orders)
+        {
+            return orders.Select(o => new OrderViewModel()
+            {
+                CustomerId = o.CustomerId,
+                LocationId = o.LocationId,
+                Date = o.Date,
+                OrderTotal = o.OrderTotal,
+                OrderNumber = o.OrderNumber
+            }).ToList();
+        }
+    }
+}
