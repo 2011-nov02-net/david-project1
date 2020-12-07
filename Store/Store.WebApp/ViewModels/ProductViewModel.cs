3334c6f15a97e97b41155ff1d7be6594d4d5ab7b
David Barnes <david.barnes0402@gmail.com>
2020-12-07 11:29:05 -0500

    Add a product view model for validation

---
+++ Store/Store.WebApp/ViewModels/ProductViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Store.WebApp.ViewModels
+{
+    public class ProductViewModel
+    {
+        [Display(Name = "Product Name")]
+        [Required, RegularExpression("[A-Z].*")]
+        public string ProductName { get; set; }
+
+        [Display(Name = "Product Description")]
+        [Required, RegularExpression("[A-Z].*")]
+        public string Description { get; set; }
+        [Display(Name = "Product Name")]
+        [Required, Range(0.01, 9999999999999999.99)]
+        public decimal Price { get; set; }
+        [Display(Name = "Order Limit of Item")]
+        [Required, Range(1, 9999999999999999)]
+        public int OrderLimit { get; set; }
+    }
+}
